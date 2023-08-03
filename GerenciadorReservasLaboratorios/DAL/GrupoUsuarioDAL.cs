c87738ab3e128eb6d57f173d2cbaf419fffa5079
Leandro Sousa <leandrogomyys@gmail.com>
2023-08-03 19:53:04 -0300

    Método Inserir - GrupoUsuarioDAL
    Foi adicionado o método Inserir

--- GerenciadorReservasLaboratorios/DAL/GrupoUsuarioDAL.cs
+++ GerenciadorReservasLaboratorios/DAL/GrupoUsuarioDAL.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    internal class GrupoUsuarioDAL
+    public class GrupoUsuarioDAL
     {
+        public void Inserir(GrupoUsuario _grupoUsuario)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = "INSERT INTO GrupoUsuario (NomeGrupo) VALUES (@NomeGrupo)";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@NomeGrupo", _grupoUsuario.NomeGrupo);
+
+                cmd.Connection = cn;    
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Ocorreu erro ao tentar Inserir um grupo de usuário no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
     }
 }
