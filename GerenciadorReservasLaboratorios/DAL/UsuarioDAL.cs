c172631216bd3a1d7473dc7305d3a669f4813472
Leandro Sousa <leandrogomyys@gmail.com>
2023-08-02 21:12:17 -0300

    MÉTODO INSERIR - UsuarioDAL
    Foi inserido o método inserir na UsuarioDAL

--- GerenciadorReservasLaboratorios/DAL/UsuarioDAL.cs
+++ GerenciadorReservasLaboratorios/DAL/UsuarioDAL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,38 @@ namespace DAL
 {
     public class UsuarioDAL
     {
+        public void Inserir(Usuario _usuario)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO Usuario(Nome, NomeUsuario, Email, Senha, Matricula, Ativo) 
+                                    VALUES(@Nome, @NomeUsuario, @Email, @Senha, @Matricula, @Ativo)";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Nome", _usuario.Nome);
+                cmd.Parameters.AddWithValue("@NomeUsuario", _usuario.NomeUsuario);
+                cmd.Parameters.AddWithValue("@Email", _usuario.Email);
+                cmd.Parameters.AddWithValue("@Senha", _usuario.Senha);
+                cmd.Parameters.AddWithValue("@Matricula", _usuario.Matricula);
+                cmd.Parameters.AddWithValue("@Ativo", _usuario.Ativo);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar inserir um usuário no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
 
     }
 }
