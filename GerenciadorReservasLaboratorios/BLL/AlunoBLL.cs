cf1b091d44a93cee91b8aa6d14ccf653bdaed1cc
JORGEHRB7 <123516648+JORGEHRB7@users.noreply.github.com>
2023-08-03 21:09:16 -0300

    Update AlunoBLL e AlunoDAL

---
+++ GerenciadorReservasLaboratorios/BLL/AlunoBLL.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL;
+using Models;
+
+namespace BLL
+{
+    public class AlunoBLL
+    {
+        private AlunoDAL alunoDAL;
+
+        public AlunoBLL()
+        {
+            alunoDAL = new AlunoDAL();
+        }
+
+        public void Inserir(Aluno aluno)
+        {
+            try
+            {
+                alunoDAL.Inserir(aluno);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar inserir o aluno.", ex);
+            }
+        }
+
+        public List<Aluno> BuscarTodos()
+        {
+            try
+            {
+                return alunoDAL.BuscarTodos();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar todos os alunos.", ex);
+            }
+        }
+
+        public Aluno BuscarPorId(int id)
+        {
+            try
+            {
+                return alunoDAL.BuscarPorId(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar o aluno por ID.", ex);
+            }
+        }
+
+        public List<Aluno> BuscarPorNome(string nome)
+        {
+            try
+            {
+                return alunoDAL.BuscarPorNome(nome);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar o aluno por nome.", ex);
+            }
+        }
+
+        public void Alterar(Aluno aluno)
+        {
+            try
+            {
+                alunoDAL.Alterar(aluno);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar alterar o aluno.", ex);
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            try
+            {
+                alunoDAL.Excluir(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar excluir o aluno.", ex);
+            }
+        }
+    }
+}
+
--- GerenciadorReservasLaboratorios/DAL/AlunoDAL.cs
+++ GerenciadorReservasLaboratorios/DAL/AlunoDAL.cs
@@ -1,12 +1,239 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DAL
 {
-    internal class AlunoDAL
+    public class AlunoDAL
     {
+        public void Inserir(Aluno aluno)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO Aluno (Nome, Matricula, Email)
+                                    VALUES (@Nome, @Matricula, @Email)";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Nome", aluno.Nome);
+                cmd.Parameters.AddWithValue("@Matricula", aluno.Matricula);
+                cmd.Parameters.AddWithValue("@Email", aluno.Email);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar inserir um aluno no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        public List<Aluno> BuscarTodos()
+        {
+            List<Aluno> alunos = new List<Aluno>();
+            Aluno aluno;
+
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "SELECT Id, Nome, Matricula, Email FROM Aluno";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cn.Open();
+
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    while (rd.Read())
+                    {
+                        aluno = new Aluno();
+                        aluno.Id = Convert.ToInt32(rd["Id"]);
+                        aluno.Nome = rd["Nome"].ToString();
+                        aluno.Matricula = rd["Matricula"].ToString();
+                        aluno.Email = rd["Email"].ToString();
+
+                        alunos.Add(aluno);
+                    }
+                }
+                return alunos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar todos os alunos no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        public Aluno BuscarPorId(int id)
+        {
+            Aluno aluno = new Aluno();
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "SELECT Id, Nome, Matricula, Email FROM Aluno WHERE Id = @Id";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Id", id);
+                cn.Open();
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    if (rd.Read())
+                    {
+                        aluno.Id = Convert.ToInt32(rd["Id"]);
+                        aluno.Nome = rd["Nome"].ToString();
+                        aluno.Matricula = rd["Matricula"].ToString();
+                        aluno.Email = rd["Email"].ToString();
+                    }
+                }
+                return aluno;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar um aluno no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        public Aluno BuscarPorMatricula(string matricula)
+        {
+            Aluno aluno = new Aluno();
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "SELECT Id, Nome, Matricula, Email FROM Aluno WHERE Matricula = @Matricula";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Matricula", matricula);
+                cn.Open();
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    if (rd.Read())
+                    {
+                        aluno.Id = Convert.ToInt32(rd["Id"]);
+                        aluno.Nome = rd["Nome"].ToString();
+                        aluno.Matricula = rd["Matricula"].ToString();
+                        aluno.Email = rd["Email"].ToString();
+                    }
+                }
+                return aluno;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar um aluno por matrícula no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        public List<Aluno> BuscarPorNome(string nome)
+        {
+            List<Aluno> alunos = new List<Aluno>();
+            Aluno aluno;
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "SELECT Id, Nome, Matricula, Email FROM Aluno WHERE Nome LIKE @Nome";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Nome", "%" + nome + "%");
+                cn.Open();
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    while (rd.Read())
+                    {
+                        aluno = new Aluno();
+                        aluno.Id = Convert.ToInt32(rd["Id"]);
+                        aluno.Nome = rd["Nome"].ToString();
+                        aluno.Matricula = rd["Matricula"].ToString();
+                        aluno.Email = rd["Email"].ToString();
+                        alunos.Add(aluno);
+                    }
+                }
+                return alunos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar alunos por nome no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        public void Alterar(Aluno aluno)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"UPDATE Aluno SET Nome = @Nome, Matricula = @Matricula, Email = @Email WHERE Id = @Id";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Nome", aluno.Nome);
+                cmd.Parameters.AddWithValue("@Matricula", aluno.Matricula);
+                cmd.Parameters.AddWithValue("@Email", aluno.Email);
+                cmd.Parameters.AddWithValue("@Id", aluno.Id);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar alterar um aluno no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = "DELETE FROM Aluno WHERE Id = @Id";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar excluir um aluno do banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
     }
 }
+
