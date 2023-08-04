581133db64a9e290bc2c8b2ff3264f256f59ea02
Leandro Sousa <leandrogomyys@gmail.com>
2023-08-04 19:57:08 -0300

    Classe CursoBLL e Método Inserir
    Foi criado a classe CursoBLL e adicionado o método Inserir na Classe CursoBLL

---
+++ GerenciadorReservasLaboratorios/BLL/CursoBLL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class CursoBLL
+    {
+        private CursoDAL cursoDAL;
+
+        public GrupoUsuarioBLL()
+        {
+            cursoDAL = new CursoDAL();
+        }
+        public void Inserir(Curso curso)
+        {
+            try
+            {
+                cursoDAL.Inserir(curso);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar inserir Curso.", ex);
+            }
+        }
+    }
+}
