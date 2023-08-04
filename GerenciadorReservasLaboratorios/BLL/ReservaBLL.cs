c55355720e5f080c764e52a62f134b1dd39c0c19
Claudio Martins Da Silva <123429011+kllaws@users.noreply.github.com>
2023-08-03 21:45:31 -0300

    Update CursoDAL.cs e ReservaBLL.cs

---
+++ GerenciadorReservasLaboratorios/BLL/ReservaBLL.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL;
+using Models;
+
+namespace BLL
+{
+    public class ReservaBLL
+    {
+        private ReservaDAL reservaDAL;
+
+        public ReservaBLL()
+        {
+            reservaDAL = new ReservaDAL();
+        }
+
+        public void Inserir(Reserva reserva)
+        {
+            try
+            {
+                reservaDAL.Inserir(reserva);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar inserir a reserva.", ex);
+            }
+        }
+
+        public List<Reserva> BuscarTodos()
+        {
+            try
+            {
+                return reservaDAL.BuscarTodos();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar todas as reservas.", ex);
+            }
+        }
+
+        public Reserva BuscarPorId(int id)
+        {
+            try
+            {
+                return reservaDAL.BuscarPorId(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar a reserva por ID.", ex);
+            }
+        }
+
+        public void Alterar(Reserva reserva)
+        {
+            try
+            {
+                reservaDAL.Alterar(reserva);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar alterar a reserva.", ex);
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            try
+            {
+                reservaDAL.Excluir(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar excluir a reserva.", ex);
+            }
+        }
+    }
+}
+
--- GerenciadorReservasLaboratorios/DAL/CursoDAL.cs
+++ GerenciadorReservasLaboratorios/DAL/CursoDAL.cs
@@ -1,12 +1,193 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DAL
 {
-    internal class CursoDAL
+    public class CursoDAL
     {
+        public void Inserir(Curso curso)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO Curso (Nome) VALUES (@Nome)";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Nome", curso.Nome);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar inserir um curso no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        public List<Curso> BuscarTodos()
+        {
+            List<Curso> cursos = new List<Curso>();
+            Curso curso;
+
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "SELECT Id, Nome FROM Curso";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cn.Open();
+
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    while (rd.Read())
+                    {
+                        curso = new Curso();
+                        curso.Id = Convert.ToInt32(rd["Id"]);
+                        curso.Nome = rd["Nome"].ToString();
+
+                        cursos.Add(curso);
+                    }
+                }
+                return cursos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar todos os cursos no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        public Curso BuscarPorId(int id)
+        {
+            Curso curso = new Curso();
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "SELECT Id, Nome FROM Curso WHERE Id = @Id";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Id", id);
+                cn.Open();
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    if (rd.Read())
+                    {
+                        curso.Id = Convert.ToInt32(rd["Id"]);
+                        curso.Nome = rd["Nome"].ToString();
+                    }
+                }
+                return curso;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar um curso no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        public Curso BuscarPorNome(string nome)
+        {
+            Curso curso = new Curso();
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "SELECT Id, Nome FROM Curso WHERE Nome = @Nome";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Nome", nome);
+                cn.Open();
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    if (rd.Read())
+                    {
+                        curso.Id = Convert.ToInt32(rd["Id"]);
+                        curso.Nome = rd["Nome"].ToString();
+                    }
+                }
+                return curso;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar um curso por nome no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        public void Alterar(Curso curso)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = "UPDATE Curso SET Nome = @Nome WHERE Id = @Id";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Nome", curso.Nome);
+                cmd.Parameters.AddWithValue("@Id", curso.Id);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar alterar um curso no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = "DELETE FROM Curso WHERE Id = @Id";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar excluir um curso do banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
     }
 }
+
+
+
