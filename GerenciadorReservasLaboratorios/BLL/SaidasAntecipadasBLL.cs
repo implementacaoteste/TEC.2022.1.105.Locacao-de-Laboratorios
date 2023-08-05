3c4c1dc457bc5b5ee084f382e1b383985f25dec4
A.Barbosa <18252705+arbarbosa1979@users.noreply.github.com>
2023-08-04 21:54:49 -0300

    Update DisciplinaBLL, PermissaoBLL, SaidasAntecipadasBLL, SalaBLL

---
+++ GerenciadorReservasLaboratorios/BLL/SaidasAntecipadasBLL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL;
+using Models;
+
+namespace BLL
+{
+    public class SaidasAntecipadasBLL
+    {
+        public void Inserir(SaidasAntecipadas saidasAntecipadas)
+        {
+            new SaidasAntecipadasDAL().Inserir(saidasAntecipadas);
+        }
+
+        public List<SaidasAntecipadas> BuscarTodos()
+        {
+           return new SaidasAntecipadasDAL().BuscarTodos();
+        }
+
+        public SaidasAntecipadas BuscarPorId(int _id)
+        {
+            return new SaidasAntecipadasDAL().BuscarPorId(_id);
+        }
+
+        public void Alterar(SaidasAntecipadas _saidasAntecipadas)
+        {
+            new SaidasAntecipadasDAL().Alterar(_saidasAntecipadas);
+        }
+
+        public void Excluir(int _id)
+        {
+            new SaidasAntecipadasDAL().Excluir(_id);
+        }
+    }
+}
+
+
---
+++ GerenciadorReservasLaboratorios/BLL/SalaBLL.cs
@@ -0,0 +1,30 @@
+﻿using DAL;
+using Models;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public class SalaBLL
+    {
+        public void Inserir(Sala _sala)
+        {
+            new SalaDAL().Inserir(_sala);
+        }
+        public List<Sala> BuscarTodos()
+        {
+            return new SalaDAL().BuscarTodos();
+        }
+        public Sala BuscarPorId(int _id)
+        {
+            return new SalaDAL().BuscarPorId(_id);
+        }
+        public void Alterar(Sala _sala)
+        {
+            new SalaDAL().Alterar(_sala);
+        }
+        public void Excluir(int _id)
+        {
+            new SalaDAL().Excluir(_id);
+        }
+    }
+}
