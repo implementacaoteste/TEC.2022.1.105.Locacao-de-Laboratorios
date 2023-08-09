b37e47752e0a42bb04c1557a56c6962ee774af76
Leandro Sousa <leandrogomyys@gmail.com>
2023-08-08 22:01:58 -0300

    Criação do FromCadastroPredio
    Com erro no BindingSource do botão salvar.

--- GerenciadorReservasLaboratorios/UILGerenReservasLab/FormCadastroPredio.Designer.cs
+++ GerenciadorReservasLaboratorios/UILGerenReservasLab/FormCadastroPredio.Designer.cs
@@ -33,13 +33,6 @@
             this.lblNomePredio = new System.Windows.Forms.Label();
             this.btnNovo = new System.Windows.Forms.Button();
             this.btnSalvar = new System.Windows.Forms.Button();
-            this.btnExcluir = new System.Windows.Forms.Button();
-            this.btnAlterar = new System.Windows.Forms.Button();
-            this.btnBuscar = new System.Windows.Forms.Button();
-            this.lblBuscar = new System.Windows.Forms.Label();
-            this.txtBuscar = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -56,7 +49,7 @@
             // txtNomePredio
             // 
             this.txtNomePredio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNomePredio.Location = new System.Drawing.Point(199, 87);
+            this.txtNomePredio.Location = new System.Drawing.Point(198, 154);
             this.txtNomePredio.Name = "txtNomePredio";
             this.txtNomePredio.Size = new System.Drawing.Size(435, 27);
             this.txtNomePredio.TabIndex = 1;
@@ -65,7 +58,7 @@
             // 
             this.lblNomePredio.AutoSize = true;
             this.lblNomePredio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNomePredio.Location = new System.Drawing.Point(74, 92);
+            this.lblNomePredio.Location = new System.Drawing.Point(73, 154);
             this.lblNomePredio.Name = "lblNomePredio";
             this.lblNomePredio.Size = new System.Drawing.Size(119, 22);
             this.lblNomePredio.TabIndex = 2;
@@ -74,17 +67,18 @@
             // btnNovo
             // 
             this.btnNovo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNovo.Location = new System.Drawing.Point(78, 202);
+            this.btnNovo.Location = new System.Drawing.Point(574, 321);
             this.btnNovo.Name = "btnNovo";
             this.btnNovo.Size = new System.Drawing.Size(96, 28);
             this.btnNovo.TabIndex = 3;
-            this.btnNovo.Text = "&Novo";
+            this.btnNovo.Text = "&Cancelar";
             this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
             // 
             // btnSalvar
             // 
             this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalvar.Location = new System.Drawing.Point(185, 202);
+            this.btnSalvar.Location = new System.Drawing.Point(458, 321);
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(96, 28);
             this.btnSalvar.TabIndex = 3;
@@ -92,84 +86,19 @@
             this.btnSalvar.UseVisualStyleBackColor = true;
             this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
-            // btnExcluir
-            // 
-            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExcluir.Location = new System.Drawing.Point(399, 202);
-            this.btnExcluir.Name = "btnExcluir";
-            this.btnExcluir.Size = new System.Drawing.Size(96, 28);
-            this.btnExcluir.TabIndex = 3;
-            this.btnExcluir.Text = "&Excluir";
-            this.btnExcluir.UseVisualStyleBackColor = true;
-            // 
-            // btnAlterar
-            // 
-            this.btnAlterar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAlterar.Location = new System.Drawing.Point(290, 202);
-            this.btnAlterar.Name = "btnAlterar";
-            this.btnAlterar.Size = new System.Drawing.Size(96, 28);
-            this.btnAlterar.TabIndex = 3;
-            this.btnAlterar.Text = "&Alterar";
-            this.btnAlterar.UseVisualStyleBackColor = true;
-            // 
-            // btnBuscar
-            // 
-            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBuscar.Location = new System.Drawing.Point(507, 202);
-            this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(93, 28);
-            this.btnBuscar.TabIndex = 3;
-            this.btnBuscar.Text = "&Buscar";
-            this.btnBuscar.UseVisualStyleBackColor = true;
-            // 
-            // lblBuscar
-            // 
-            this.lblBuscar.AutoSize = true;
-            this.lblBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBuscar.Location = new System.Drawing.Point(74, 155);
-            this.lblBuscar.Name = "lblBuscar";
-            this.lblBuscar.Size = new System.Drawing.Size(71, 22);
-            this.lblBuscar.TabIndex = 2;
-            this.lblBuscar.Text = "Buscar:";
-            // 
-            // txtBuscar
-            // 
-            this.txtBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBuscar.Location = new System.Drawing.Point(151, 150);
-            this.txtBuscar.Name = "txtBuscar";
-            this.txtBuscar.Size = new System.Drawing.Size(449, 27);
-            this.txtBuscar.TabIndex = 1;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(78, 250);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(556, 217);
-            this.dataGridView1.TabIndex = 4;
-            // 
             // FormCadastroPredio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(763, 509);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.btnAlterar);
-            this.Controls.Add(this.btnBuscar);
-            this.Controls.Add(this.btnExcluir);
+            this.ClientSize = new System.Drawing.Size(710, 379);
             this.Controls.Add(this.btnSalvar);
             this.Controls.Add(this.btnNovo);
-            this.Controls.Add(this.lblBuscar);
             this.Controls.Add(this.lblNomePredio);
-            this.Controls.Add(this.txtBuscar);
             this.Controls.Add(this.txtNomePredio);
             this.Controls.Add(this.label1);
             this.Name = "FormCadastroPredio";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormCadastroPredio";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,16 +106,11 @@
 
         #endregion
 
+        
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtNomePredio;
         private System.Windows.Forms.Label lblNomePredio;
         private System.Windows.Forms.Button btnNovo;
         private System.Windows.Forms.Button btnSalvar;
-        private System.Windows.Forms.Button btnExcluir;
-        private System.Windows.Forms.Button btnAlterar;
-        private System.Windows.Forms.Button btnBuscar;
-        private System.Windows.Forms.Label lblBuscar;
-        private System.Windows.Forms.TextBox txtBuscar;
-        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- GerenciadorReservasLaboratorios/UILGerenReservasLab/FormCadastroPredio.cs
+++ GerenciadorReservasLaboratorios/UILGerenReservasLab/FormCadastroPredio.cs
@@ -15,9 +15,11 @@ namespace UILGerenReservasLab
 {
     public partial class FormCadastroPredio : Form
     {
-        public FormCadastroPredio()
+        public int Id;
+        public FormCadastroPredio(int _id = 0)
         {
             InitializeComponent();
+            Id = _id;
         }
 
         private void btnSalvar_Click(object sender, EventArgs e)
@@ -29,10 +31,39 @@ namespace UILGerenReservasLab
                 txtNomePredio.Focus();
                 return;
             }
+            try
+            {
+                predioBindingSource.EndEdit();
+
+                Predio predio = ((Predio)predioBindingSource.Current, txtNomePredio.Text);
+                if (Id == 0)
+                    new PredioBLL().Inserir(predio);
+                else
+                    new PredioBLL().Alterar(predio);
+                MessageBox.Show("Registro salvo com sucesso!");
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
 
         }
 
-       
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
 
