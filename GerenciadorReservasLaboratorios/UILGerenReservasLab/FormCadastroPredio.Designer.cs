4d135b8322563610f8f6c659c71a2af880564fc0
Leandro Sousa <leandrogomyys@gmail.com>
2023-08-08 21:05:14 -0300

    Update FormCadastroPredio

--- GerenciadorReservasLaboratorios/UILGerenReservasLab/FormCadastroPredio.Designer.cs
+++ GerenciadorReservasLaboratorios/UILGerenReservasLab/FormCadastroPredio.Designer.cs
@@ -101,7 +101,6 @@
             this.btnExcluir.TabIndex = 3;
             this.btnExcluir.Text = "&Excluir";
             this.btnExcluir.UseVisualStyleBackColor = true;
-            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
             // 
             // btnAlterar
             // 
