515d55c5dbecbca87ac5b39f21ade2493afb1071
Leandro Sousa <leandrogomyys@gmail.com>
2023-08-09 21:56:39 -0300

    Criado o FormBuscarSala
    Falta implementar os códigos nos botões

---
+++ GerenciadorReservasLaboratorios/UILGerenReservasLab/FormBuscarSala.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace UILGerenReservasLab
+{
+    partial class FormBuscarSala
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblBuscarPor = new System.Windows.Forms.Label();
+            this.salaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.salaDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.salaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salaDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(363, 157);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "&Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(460, 157);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 0;
+            this.btnAlterar.Text = "&Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(558, 157);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 0;
+            this.btnInserir.Text = "&Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(653, 157);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 0;
+            this.btnExcluir.Text = "&Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(854, 52);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Buscar Por Sala ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(253, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(482, 24);
+            this.textBox1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(90, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(143, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // lblBuscarPor
+            // 
+            this.lblBuscarPor.AutoSize = true;
+            this.lblBuscarPor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuscarPor.Location = new System.Drawing.Point(87, 78);
+            this.lblBuscarPor.Name = "lblBuscarPor";
+            this.lblBuscarPor.Size = new System.Drawing.Size(87, 18);
+            this.lblBuscarPor.TabIndex = 6;
+            this.lblBuscarPor.Text = "Buscar Por:";
+            // 
+            // salaBindingSource
+            // 
+            this.salaBindingSource.DataSource = typeof(Models.Sala);
+            // 
+            // salaDataGridView
+            // 
+            this.salaDataGridView.AllowUserToAddRows = false;
+            this.salaDataGridView.AllowUserToDeleteRows = false;
+            this.salaDataGridView.AllowUserToOrderColumns = true;
+            this.salaDataGridView.AutoGenerateColumns = false;
+            this.salaDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.salaDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.salaDataGridView.DataSource = this.salaBindingSource;
+            this.salaDataGridView.Location = new System.Drawing.Point(90, 196);
+            this.salaDataGridView.Name = "salaDataGridView";
+            this.salaDataGridView.ReadOnly = true;
+            this.salaDataGridView.RowHeadersWidth = 51;
+            this.salaDataGridView.RowTemplate.Height = 24;
+            this.salaDataGridView.Size = new System.Drawing.Size(645, 261);
+            this.salaDataGridView.TabIndex = 9;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.FillWeight = 53.47594F;
+            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Nome";
+            this.dataGridViewTextBoxColumn2.FillWeight = 146.5241F;
+            this.dataGridViewTextBoxColumn2.HeaderText = "Nome";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // FormBuscarSala
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 538);
+            this.Controls.Add(this.salaDataGridView);
+            this.Controls.Add(this.lblBuscarPor);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnBuscar);
+            this.Name = "FormBuscarSala";
+            this.Text = "FormBuscarSala";
+            ((System.ComponentModel.ISupportInitialize)(this.salaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salaDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblBuscarPor;
+        private System.Windows.Forms.BindingSource salaBindingSource;
+        private System.Windows.Forms.DataGridView salaDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+    }
+}
---
+++ GerenciadorReservasLaboratorios/UILGerenReservasLab/FormCadastroSala.designer.cs
@@ -0,0 +1,123 @@
+﻿namespace UILGerenReservasLab
+{
+    partial class FormCadastroSala
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label lblNomeSala;
+            this.salaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.txtNomeSala = new System.Windows.Forms.TextBox();
+            this.btnSalvarSala = new System.Windows.Forms.Button();
+            this.btnCancelarSala = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            lblNomeSala = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.salaBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNomeSala
+            // 
+            lblNomeSala.AutoSize = true;
+            lblNomeSala.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblNomeSala.Location = new System.Drawing.Point(97, 141);
+            lblNomeSala.Name = "lblNomeSala";
+            lblNomeSala.Size = new System.Drawing.Size(132, 20);
+            lblNomeSala.TabIndex = 1;
+            lblNomeSala.Text = "Nome do Sala:";
+            // 
+            // salaBindingSource
+            // 
+            this.salaBindingSource.DataSource = typeof(Models.Sala);
+            // 
+            // txtNomeSala
+            // 
+            this.txtNomeSala.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.salaBindingSource, "Nome", true));
+            this.txtNomeSala.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeSala.Location = new System.Drawing.Point(101, 182);
+            this.txtNomeSala.Name = "txtNomeSala";
+            this.txtNomeSala.Size = new System.Drawing.Size(562, 27);
+            this.txtNomeSala.TabIndex = 2;
+            // 
+            // btnSalvarSala
+            // 
+            this.btnSalvarSala.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvarSala.Location = new System.Drawing.Point(501, 374);
+            this.btnSalvarSala.Name = "btnSalvarSala";
+            this.btnSalvarSala.Size = new System.Drawing.Size(86, 33);
+            this.btnSalvarSala.TabIndex = 3;
+            this.btnSalvarSala.Text = "&Salvar";
+            this.btnSalvarSala.UseVisualStyleBackColor = true;
+            this.btnSalvarSala.Click += new System.EventHandler(this.btnSalvarSala_Click);
+            // 
+            // btnCancelarSala
+            // 
+            this.btnCancelarSala.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelarSala.Location = new System.Drawing.Point(628, 374);
+            this.btnCancelarSala.Name = "btnCancelarSala";
+            this.btnCancelarSala.Size = new System.Drawing.Size(75, 33);
+            this.btnCancelarSala.TabIndex = 3;
+            this.btnCancelarSala.Text = "&Cancelar";
+            this.btnCancelarSala.UseVisualStyleBackColor = true;
+            this.btnCancelarSala.Click += new System.EventHandler(this.btnCancelarSala_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(230, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cadastro de Salas";
+            // 
+            // FormCadastroSala
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 468);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancelarSala);
+            this.Controls.Add(this.btnSalvarSala);
+            this.Controls.Add(lblNomeSala);
+            this.Controls.Add(this.txtNomeSala);
+            this.Name = "FormCadastroSala";
+            this.Text = "FormCadastroSala";
+            ((System.ComponentModel.ISupportInitialize)(this.salaBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource salaBindingSource;
+        private System.Windows.Forms.TextBox txtNomeSala;
+        private System.Windows.Forms.Button btnSalvarSala;
+        private System.Windows.Forms.Button btnCancelarSala;
+        private System.Windows.Forms.Label label1;
+    }
+}
