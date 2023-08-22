04e7471888ec21b6bd0d6c8720534e6a7a4b8926
A.Barbosa <18252705+arbarbosa1979@users.noreply.github.com>
2023-08-22 19:31:57 -0300

    Update csproj

---
+++ IMG/addforms/FormMenu.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace UILGerenReservasLab
+{
+    partial class FormMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.buttonSala = new System.Windows.Forms.Button();
+            this.buttonSaidasAntecipadas = new System.Windows.Forms.Button();
+            this.buttonReserva = new System.Windows.Forms.Button();
+            this.buttonPredio = new System.Windows.Forms.Button();
+            this.buttonPermissao = new System.Windows.Forms.Button();
+            this.buttonGrupoUsuario = new System.Windows.Forms.Button();
+            this.buttonDisciplina = new System.Windows.Forms.Button();
+            this.buttonCurso = new System.Windows.Forms.Button();
+            this.buttonAluno = new System.Windows.Forms.Button();
+            this.buttonUsuario = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panelMenu.Controls.Add(this.buttonSala);
+            this.panelMenu.Controls.Add(this.buttonSaidasAntecipadas);
+            this.panelMenu.Controls.Add(this.buttonReserva);
+            this.panelMenu.Controls.Add(this.buttonPredio);
+            this.panelMenu.Controls.Add(this.buttonPermissao);
+            this.panelMenu.Controls.Add(this.buttonGrupoUsuario);
+            this.panelMenu.Controls.Add(this.buttonDisciplina);
+            this.panelMenu.Controls.Add(this.buttonCurso);
+            this.panelMenu.Controls.Add(this.buttonAluno);
+            this.panelMenu.Controls.Add(this.buttonUsuario);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 561);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // buttonSala
+            // 
+            this.buttonSala.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonSala.FlatAppearance.BorderSize = 0;
+            this.buttonSala.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSala.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSala.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonSala.Location = new System.Drawing.Point(0, 460);
+            this.buttonSala.Name = "buttonSala";
+            this.buttonSala.Size = new System.Drawing.Size(220, 40);
+            this.buttonSala.TabIndex = 10;
+            this.buttonSala.Text = "Salas";
+            this.buttonSala.UseVisualStyleBackColor = true;
+            // 
+            // buttonSaidasAntecipadas
+            // 
+            this.buttonSaidasAntecipadas.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonSaidasAntecipadas.FlatAppearance.BorderSize = 0;
+            this.buttonSaidasAntecipadas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSaidasAntecipadas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSaidasAntecipadas.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonSaidasAntecipadas.Location = new System.Drawing.Point(0, 420);
+            this.buttonSaidasAntecipadas.Name = "buttonSaidasAntecipadas";
+            this.buttonSaidasAntecipadas.Size = new System.Drawing.Size(220, 40);
+            this.buttonSaidasAntecipadas.TabIndex = 9;
+            this.buttonSaidasAntecipadas.Text = "Saídas Antecipadas";
+            this.buttonSaidasAntecipadas.UseVisualStyleBackColor = true;
+            this.buttonSaidasAntecipadas.Click += new System.EventHandler(this.buttonSaidasAntecipadas_Click);
+            // 
+            // buttonReserva
+            // 
+            this.buttonReserva.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonReserva.FlatAppearance.BorderSize = 0;
+            this.buttonReserva.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonReserva.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReserva.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonReserva.Location = new System.Drawing.Point(0, 380);
+            this.buttonReserva.Name = "buttonReserva";
+            this.buttonReserva.Size = new System.Drawing.Size(220, 40);
+            this.buttonReserva.TabIndex = 8;
+            this.buttonReserva.Text = "Reservas";
+            this.buttonReserva.UseVisualStyleBackColor = true;
+            this.buttonReserva.Click += new System.EventHandler(this.buttonReserva_Click);
+            // 
+            // buttonPredio
+            // 
+            this.buttonPredio.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonPredio.FlatAppearance.BorderSize = 0;
+            this.buttonPredio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPredio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPredio.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonPredio.Location = new System.Drawing.Point(0, 340);
+            this.buttonPredio.Name = "buttonPredio";
+            this.buttonPredio.Size = new System.Drawing.Size(220, 40);
+            this.buttonPredio.TabIndex = 7;
+            this.buttonPredio.Text = "Predios";
+            this.buttonPredio.UseVisualStyleBackColor = true;
+            // 
+            // buttonPermissao
+            // 
+            this.buttonPermissao.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonPermissao.FlatAppearance.BorderSize = 0;
+            this.buttonPermissao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPermissao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPermissao.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonPermissao.Location = new System.Drawing.Point(0, 300);
+            this.buttonPermissao.Name = "buttonPermissao";
+            this.buttonPermissao.Size = new System.Drawing.Size(220, 40);
+            this.buttonPermissao.TabIndex = 6;
+            this.buttonPermissao.Text = "Permissões";
+            this.buttonPermissao.UseVisualStyleBackColor = true;
+            // 
+            // buttonGrupoUsuario
+            // 
+            this.buttonGrupoUsuario.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonGrupoUsuario.FlatAppearance.BorderSize = 0;
+            this.buttonGrupoUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGrupoUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGrupoUsuario.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonGrupoUsuario.Location = new System.Drawing.Point(0, 260);
+            this.buttonGrupoUsuario.Name = "buttonGrupoUsuario";
+            this.buttonGrupoUsuario.Size = new System.Drawing.Size(220, 40);
+            this.buttonGrupoUsuario.TabIndex = 5;
+            this.buttonGrupoUsuario.Text = "Grupos de Usuário";
+            this.buttonGrupoUsuario.UseVisualStyleBackColor = true;
+            // 
+            // buttonDisciplina
+            // 
+            this.buttonDisciplina.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonDisciplina.FlatAppearance.BorderSize = 0;
+            this.buttonDisciplina.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDisciplina.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDisciplina.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonDisciplina.Location = new System.Drawing.Point(0, 220);
+            this.buttonDisciplina.Name = "buttonDisciplina";
+            this.buttonDisciplina.Size = new System.Drawing.Size(220, 40);
+            this.buttonDisciplina.TabIndex = 4;
+            this.buttonDisciplina.Text = "Disciplina";
+            this.buttonDisciplina.UseVisualStyleBackColor = true;
+            this.buttonDisciplina.Click += new System.EventHandler(this.buttonDisciplina_Click);
+            // 
+            // buttonCurso
+            // 
+            this.buttonCurso.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonCurso.FlatAppearance.BorderSize = 0;
+            this.buttonCurso.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCurso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCurso.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonCurso.Location = new System.Drawing.Point(0, 180);
+            this.buttonCurso.Name = "buttonCurso";
+            this.buttonCurso.Size = new System.Drawing.Size(220, 40);
+            this.buttonCurso.TabIndex = 3;
+            this.buttonCurso.Text = "Curso";
+            this.buttonCurso.UseVisualStyleBackColor = true;
+            this.buttonCurso.Click += new System.EventHandler(this.buttonCurso_Click);
+            // 
+            // buttonAluno
+            // 
+            this.buttonAluno.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonAluno.FlatAppearance.BorderSize = 0;
+            this.buttonAluno.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAluno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAluno.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonAluno.Location = new System.Drawing.Point(0, 140);
+            this.buttonAluno.Name = "buttonAluno";
+            this.buttonAluno.Size = new System.Drawing.Size(220, 40);
+            this.buttonAluno.TabIndex = 2;
+            this.buttonAluno.Text = "Aluno";
+            this.buttonAluno.UseVisualStyleBackColor = true;
+            // 
+            // buttonUsuario
+            // 
+            this.buttonUsuario.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonUsuario.FlatAppearance.BorderSize = 0;
+            this.buttonUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUsuario.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonUsuario.Location = new System.Drawing.Point(0, 100);
+            this.buttonUsuario.Name = "buttonUsuario";
+            this.buttonUsuario.Size = new System.Drawing.Size(220, 40);
+            this.buttonUsuario.TabIndex = 1;
+            this.buttonUsuario.Text = "Usuario";
+            this.buttonUsuario.UseVisualStyleBackColor = true;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.SystemColors.ControlText;
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 100);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // FormMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 561);
+            this.Controls.Add(this.panelMenu);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormMenu";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.FormMenu_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button buttonUsuario;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Button buttonSala;
+        private System.Windows.Forms.Button buttonSaidasAntecipadas;
+        private System.Windows.Forms.Button buttonReserva;
+        private System.Windows.Forms.Button buttonPredio;
+        private System.Windows.Forms.Button buttonPermissao;
+        private System.Windows.Forms.Button buttonGrupoUsuario;
+        private System.Windows.Forms.Button buttonDisciplina;
+        private System.Windows.Forms.Button buttonCurso;
+        private System.Windows.Forms.Button buttonAluno;
+    }
+}
