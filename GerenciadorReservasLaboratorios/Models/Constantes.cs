573d2f6227bafe1261c4b9af54528caeb5c28d3f
A.Barbosa <18252705+arbarbosa1979@users.noreply.github.com>
2023-10-03 21:08:28 -0300

    Update Constantes.cs

--- GerenciadorReservasLaboratorios/Models/Constantes.cs
+++ GerenciadorReservasLaboratorios/Models/Constantes.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,20 @@ namespace Models
         public static int IdUsuarioLogado;
         public static string StringDeConexao;
         public static string DiretorioStringConexao = "C:\\Configuracao\\";
+        public static string CaminhoChavePublica = "C:\Configuracao\\ChavePublica.txt"
+
+        public static string CaminhoChavePrivada = "C:\\Configuracao\\ChavePrivada.txt"
         public static string NomeArquivoConexao = "sgreservasConnection.config";
+        public static string DiretorioArquivoLog = Environment.CurrentDirectory + "\\Logs\\";
+
+        public static string CaminhoArquivoLog
+        {
+            get
+            {
+                Directory.CreateDirectory(DiretorioArquivoLog);
+                return DiretorioArquivoLog + "Log" + DateTime.Now.Year.ToString() +
+                DateTime.Now.Month.ToString("00") + DateTime.Now.Day.ToString("00") + ".log";
+            }
+        }
     }
 }
